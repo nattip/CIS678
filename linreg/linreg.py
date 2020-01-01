7ca6e8df54a4199806271562499eb9f3d642e3ba
Natalie Tipton <tiptonna@mail.gvsu.edu>
2020-01-01 15:56:28 -0500

    Scaffold for project one

---
+++ linreg/linreg.py
@@ -0,0 +1,38 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# https://seaborn.pydata.org/tutorial/regression.html use this later
+
+class LinReg:
+    def __init__(self, in_data):
+        self.in_data = in_data
+    
+    def process(self):
+        """
+        This cleans up the data so the regressor
+        can read it
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        pass
+
+    def reg(self):
+        """
+        regress over the data and fit a line
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        pass
