9c9099b2267368033eca450aca42e4c16d4cad7b
Natalie Tipton <tiptonna@mail.gvsu.edu>
2020-02-08 17:58:55 -0500

    working model

--- project_2/bayes.py
+++ project_2/bayes.py
@@ -6,6 +6,7 @@
 
 from collections import Counter
 import string
+import math
 
 #################################################
 # Function to clean incoming data:
@@ -40,20 +41,25 @@ if __name__ == "__main__":
 
     # Unique words for ham or spam
     all_words = []
-    total_unique_words = set()
+    vocab = set()
 
     # Probabilities of words in each class
-    prob_ham = []
-    prob_spam = []
+    prob_ham = {}
+    prob_spam = {}
+
+    # initial counts for number of ham/spam messages
+    ham_count = 0
+    spam_count = 0
 
     # read in file one line at a time
-    with open("./data.data") as f:
+    with open("./train.data") as f:
         lines = f.readlines()
 
         # Read all of the lines and append them to their classification
         for line in lines:
             if line.startswith("ham"):
                 # Get the whole string
+                ham_count += 1
                 raw_text = "".join(line.split("ham"))[1:].rstrip()
                 # split string by spaces and append each word appropriately
                 for val in raw_text.split(" "):
@@ -61,12 +67,13 @@ if __name__ == "__main__":
                     ham_raw.append(val)
 
             if line.startswith("spam"):
+                spam_count += 1
                 raw_text = "".join(line.split("spam"))[1:].rstrip()
                 for val in raw_text.split(" "):
                     all_words.append(val)
                     spam_raw.append(val)
 
-        # sort words into different places
+        # clean words in all classifications
         cleaned_words = clean_words(all_words)
         cleaned_ham_words = clean_words(ham_raw)
         cleaned_spam_words = clean_words(spam_raw)
@@ -76,32 +83,104 @@ if __name__ == "__main__":
         cleaned_ham_word_counts = Counter(cleaned_ham_words)
         cleaned_spam_word_counts = Counter(cleaned_spam_words)
 
-        # count number of words in all messages
-        total_words_with_duplicates = len(cleaned_words)
+        # create vocabulary with no repeating words
+        vocab = set(cleaned_words)
 
-        # print(cleaned_spam_words_counts["hi"], cleaned_words_counts["hi"])
-        total_unique_words = set(cleaned_words)
+        # remove 5 most common words in all messages
+        vocab.remove("to")
+        vocab.remove("i")
+        vocab.remove("you")
+        vocab.remove("a")
+        vocab.remove("the")
 
-        # TODO figure out how to get just values from cleaned_spam/ham_word_counts
-        for word in range(len(total_unique_words)):
-            prob_spam[0] = (cleaned_spam_word_counts[0] + 1) / (
-                len(total_words_with_duplicates) + len(total_unique_words)
+        # find sizes of all different sets of words
+        vocab_size = len(vocab)
+        spam_size = len(cleaned_spam_words)
+        ham_size = len(cleaned_ham_words)
+
+        # make dictionaries of spam and ham words with the probability
+        # that each word will occur in either classification
+        for word in vocab:
+            prob_spam[word] = (cleaned_spam_word_counts[word] + 1) / (
+                spam_size + vocab_size
             )
-            prob_ham[0] = (cleaned_ham_word_counts[0] + 1) / (
-                total_words_with_duplicates + len(total_unique_words)
+            prob_ham[word] = (cleaned_ham_word_counts[word] + 1) / (
+                ham_size + vocab_size
             )
-
-        print(total_words_with_duplicates)
-        print(len(total_unique_words))
-        print(prob_ham)
-
-        # for uword in total_unique_words:
-        #     print(
-        #         f"occurance of word: {uword} in "
-        #         f"ham: {cleaned_ham_words_counts[uword]} in "
-        #         f"spam: {cleaned_spam_words_counts[uword]}"
-        #     )
 
         # Use this to find words that are common in everything
         # and maybe not count thoes for bayes
-        # print(cleaned_words.most_common(5))
+        # print(cleaned_words_counts.most_common(5))
+
+        prob_of_ham_message = ham_count / (ham_count + spam_count)
+        prob_of_spam_message = spam_count / (ham_count + spam_count)
+
+    # lists of true and hypothesized classifications
+    true_class = []
+    hyp_class = []
+
+    # open test data line by line
+    with open("./test.data") as f:
+        lines = f.readlines()
+
+        # go through lines 1 at a time
+        for line in lines:
+            big_product_ham = 0
+            big_product_spam = 0
+
+            # pull the true classification off the message into a list
+            true_class.append(line.split()[0])
+            # leave only the message w/o the classification remaining
+            message = line.split()[1:]
+
+            # for each word in the message
+            for word in message:
+                # do not count if word is not in vocabulary
+                if word not in vocab:
+                    continue
+
+                # naive bayes formula using log rules
+                big_product_ham += math.log10(prob_ham[word])
+                big_product_spam += math.log10(prob_spam[word])
+
+            cnb_ham = math.log10(prob_of_ham_message) + big_product_ham
+            cnb_spam = math.log10(prob_of_spam_message) + big_product_spam
+
+            # classify message
+            if cnb_ham > cnb_spam:
+                hyp_class.append("ham")
+            elif cnb_ham < cnb_spam:
+                hyp_class.append("spam")
+            else:
+                hyp_class.append("ham")
+
+        true_pos = 0
+        true_neg = 0
+        false_pos = 0
+        false_neg = 0
+
+        # count up true and false pos and negs
+        for i in range(0, len(true_class)):
+            if true_class[i] == "spam" and hyp_class[i] == "spam":
+                true_pos += 1
+            elif true_class[i] == "ham" and hyp_class[i] == "ham":
+                true_neg += 1
+            elif true_class[i] == "spam" and hyp_class[i] == "ham":
+                false_neg += 1
+            elif true_class[i] == "ham" and hyp_class[i] == "spam":
+                false_pos += 1
+
+        # calculate metrics for model
+        correct = (
+            100 * (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg)
+        )
+        sensitivity = 100 * (true_pos / (true_pos + false_neg))
+        specificity = 100 * (true_neg / (true_neg + false_pos))
+
+        print("\ntrue positives =", true_pos)
+        print("true negatives =", true_neg)
+        print("false positives =", false_pos)
+        print("false negatives =", false_neg)
+        print("\nsensitivity =", sensitivity)
+        print("specificity =", specificity)
+        print("correct classification =", correct, "\n")
