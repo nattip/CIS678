1887ac4de4d430a5257c095d56acdf1e2fad5f74
Natalie Tipton <tiptonna@mail.gvsu.edu>
2020-02-09 19:03:28 -0500

    Add graphics

--- project_2/bayes.py
+++ project_2/bayes.py
@@ -1,12 +1,16 @@
 # Written by Natalie Tipton
 # February 2, 20202
 # CIS 678 - Machine Learning
-# Spam Classification of text messages using
+# Dr. Greg Wolfefe
+# Spam/Ham Classification of text messages using
 #   the Naive Bayes algorithm
 
 from collections import Counter
 import string
 import math
+import heapq
+import matplotlib.pyplot as plt
+import numpy as np
 
 #################################################
 # Function to clean incoming data:
@@ -46,6 +50,7 @@ if __name__ == "__main__":
     # Probabilities of words in each class
     prob_ham = {}
     prob_spam = {}
+    prob_diff = {}
 
     # initial counts for number of ham/spam messages
     ham_count = 0
@@ -108,10 +113,15 @@ if __name__ == "__main__":
                 ham_size + vocab_size
             )
 
+            # find the difference between spam and ham probabilities
+            # for future analysis of most obvious classifications
+            prob_diff[word] = prob_ham[word] - prob_spam[word]
+
         # Use this to find words that are common in everything
-        # and maybe not count thoes for bayes
+        # and maybe not count those for bayes
         # print(cleaned_words_counts.most_common(5))
 
+        # calculate overall percentage of spam and ham messages in training
         prob_of_ham_message = ham_count / (ham_count + spam_count)
         prob_of_spam_message = spam_count / (ham_count + spam_count)
 
@@ -123,13 +133,26 @@ if __name__ == "__main__":
     with open("./test.data") as f:
         lines = f.readlines()
 
+        # set counters for true and hypothesized classes to 0
+        test_ham_count = 0
+        test_spam_count = 0
+        hyp_ham_count = 0
+        hyp_spam_count = 0
+
         # go through lines 1 at a time
         for line in lines:
+            # reset the big product for naive bayes calculation back to 0
             big_product_ham = 0
             big_product_spam = 0
 
             # pull the true classification off the message into a list
             true_class.append(line.split()[0])
+            # count up the true occurrences for percentage calculations
+            if line.split()[0] == "ham":
+                test_ham_count += 1
+            if line.split()[0] == "spam":
+                test_spam_count += 1
+
             # leave only the message w/o the classification remaining
             message = line.split()[1:]
 
@@ -149,10 +172,13 @@ if __name__ == "__main__":
             # classify message
             if cnb_ham > cnb_spam:
                 hyp_class.append("ham")
+                hyp_ham_count += 1
             elif cnb_ham < cnb_spam:
                 hyp_class.append("spam")
+                hyp_spam_count += 1
             else:
                 hyp_class.append("ham")
+                hyp_ham_count += 1
 
         true_pos = 0
         true_neg = 0
@@ -174,13 +200,153 @@ if __name__ == "__main__":
         correct = (
             100 * (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg)
         )
-        sensitivity = 100 * (true_pos / (true_pos + false_neg))
-        specificity = 100 * (true_neg / (true_neg + false_pos))
+        recall = 100 * (true_pos / (true_pos + false_neg))
+        tnr = 100 * (true_neg / (true_neg + false_pos))
+        precision = 100 * (true_pos / (true_pos + false_pos))
+
+        #################################################
+        # Output
+        #   Printed metrics
+        #   Pie Charts
+        #   Bar Charts
 
         print("\ntrue positives =", true_pos)
         print("true negatives =", true_neg)
         print("false positives =", false_pos)
         print("false negatives =", false_neg)
-        print("\nsensitivity =", sensitivity)
-        print("specificity =", specificity)
+        print("\nRecall =", recall)
+        print("Precision =", precision)
+        print("True Negative Rate =", tnr)
         print("correct classification =", correct, "\n")
+
+        # determining what the most telling spam and ham words are
+        # print(heapq.nlargest(5, prob_diff, key=prob_diff.get))
+        # print(heapq.nsmallest(5, prob_diff, key=prob_diff.get))
+
+        # create pie charts to show percentage of spam and ham
+        # messages in training and testing and what the model
+        # determined for the testing set
+        labels = "Spam", "Ham"
+        sizes = [prob_of_spam_message, prob_of_ham_message]
+        explode = (0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+        fig1, ax1 = plt.subplots()
+        ax1.pie(
+            sizes,
+            explode=explode,
+            labels=labels,
+            autopct="%1.1f%%",
+            shadow=True,
+            startangle=90,
+        )
+        ax1.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
+        plt.title("True Training Percentages")
+        plt.show()
+
+        labels = "Spam", "Ham"
+        sizes = [
+            test_spam_count / (test_spam_count + test_ham_count),
+            test_ham_count / (test_spam_count + test_ham_count),
+        ]
+        explode = (0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+        fig2, ax1 = plt.subplots()
+        ax1.pie(
+            sizes,
+            explode=explode,
+            labels=labels,
+            autopct="%1.1f%%",
+            shadow=True,
+            startangle=90,
+        )
+        ax1.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
+        plt.title("True Testing Percentages")
+        plt.show()
+
+        labels = "Spam", "Ham"
+        sizes = [
+            hyp_spam_count / (hyp_spam_count + hyp_ham_count),
+            hyp_ham_count / (hyp_spam_count + hyp_ham_count),
+        ]
+        explode = (0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+        fig3, ax1 = plt.subplots()
+        ax1.pie(
+            sizes,
+            explode=explode,
+            labels=labels,
+            autopct="%1.1f%%",
+            shadow=True,
+            startangle=90,
+        )
+
+        ax1.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
+        plt.title("Hypothesized Testing Percentages")
+        plt.show()
+
+        # create stacked bar charts to show the probability of spam or ham
+        # for the most telling spam words and then the most telling ham words
+        plt.figure(4)
+        N = 5
+
+        # showing probability for the words determined to be most telling for spam
+        top_spam_probs = (
+            prob_spam["call"],
+            prob_spam["free"],
+            prob_spam["txt"],
+            prob_spam["claim"],
+            prob_spam["your"],
+        )
+        low_ham_probs = (
+            prob_ham["call"],
+            prob_ham["free"],
+            prob_ham["txt"],
+            prob_ham["claim"],
+            prob_ham["your"],
+        )
+
+        ind = np.arange(N)  # the x locations for the groups
+        width = 0.35  # the width of the bars: can also be len(x) sequence
+
+        p1 = plt.bar(ind, low_ham_probs, width)
+        p2 = plt.bar(ind, top_spam_probs, width, bottom=low_ham_probs)
+
+        plt.ylabel("Probability of Word")
+        plt.title("Probability of Spam or Ham\nFor Most Telling Spam Words")
+        plt.xticks(ind, ("call", "free", "txt", "claim", "your"))
+        plt.legend((p1[0], p2[0]), ("Ham", "Spam"))
+
+        plt.show()
+
+        plt.figure(5)
+        N = 5
+
+        # showing probability for the words determined to be most telling for ham
+        low_spam_probs = (
+            prob_spam["my"],
+            prob_spam["me"],
+            prob_spam["in"],
+            prob_spam["it"],
+            prob_spam["u"],
+        )
+        top_ham_probs = (
+            prob_ham["my"],
+            prob_ham["me"],
+            prob_ham["in"],
+            prob_ham["it"],
+            prob_ham["u"],
+        )
+
+        ind = np.arange(N)  # the x locations for the groups
+        width = 0.35  # the width of the bars: can also be len(x) sequence
+
+        p1 = plt.bar(ind, low_spam_probs, width)
+        p2 = plt.bar(ind, top_ham_probs, width, bottom=low_spam_probs)
+
+        plt.ylabel("Probability of Word")
+        plt.title("Probability of Spam or Ham\nFor Most Telling Ham Words")
+        plt.xticks(ind, ("my", "me", "in", "it", "u"))
+        plt.legend((p1[0], p2[0]), ("Spam", "Ham"))
+
+        plt.show()
+
