9302302a96d470f886dd8a24d2769e54cb3b4614
Jarred Parr <jparr721@gmail.com>
2020-03-03 17:19:03 -0800

    Bitch Im a cow
    I am not a cat, I don't say meow

--- project_3/id3.py
+++ project_3/id3.py
@@ -4,24 +4,66 @@
 # Dr. Greg Wolfefe
 # ID3 decision tree creator
 
+import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
+
+class DecisionTree:
+    def __init__(self, attributes, dataset, classes, groups):
+        self.attributes = attributes
+        self.classes = classes
+        self.dataset = dataset
+        self.groups = groups
+
+    def fit(self):
+        for row in self.dataset.iterrows():
+            pass
+
 
 if __name__ == "__main__":
+    classes = {"count": 0, "values": []}
+    attributes = {"count": 0, "values": []}
 
-    targetNames = []
-    attributeNames = []
+    # Pandas dataframe to hold the dataset
+    dataset = None
 
-    with open("./fishing_data.txt") as f:
-        lines = f.readlines()
+    with open("data.txt") as f:
+        # Get Output Classes
+        # Gets the number of values
+        classes["count"] = int(f.readline().rstrip())
+        # Strips \n and \r
+        classes = f.readline().rstrip().split(",")
 
-        for line in range (0,6):
-            if line == 0:
-                numTargets = lines[line]
-            elif line == 1:
-                targetNames = lines[line].split("")
-            elif line == 2:
-                numAttributes = lines[line]
-            elif line == 3:
-                for word in lines[line]:
-                    
+        # Get Attributes
+        attributes["count"] = int(f.readline().rstrip())
+
+        if attributes["count"] > 1:
+            for i in range(attributes["count"]):
+                attributes["values"].append(
+                    f.readline().rstrip().split(",")[0]
+                )
+
+        # Make Dataset
+        num_values = int(f.readline().rstrip())
+        list_of_values = []
+
+        for i in range(num_values):
+            values = f.readline().rstrip().split(",")
+            attributes_with_classification = attributes["values"] + ["label"]
+
+            dataframe = {}
+
+            for key, value in zip(attributes_with_classification, values):
+                dataframe[key] = value
+
+            list_of_values.append(dataframe)
+            dataframe = {}
+
+        dataset = pd.DataFrame(list_of_values)
+
+    groups = {}
+    for attr in attributes["values"]:
+        groups[attr] = pd.DataFrame(dataset.groupby([attr, "label"]).size())
+        groups[attr].reset_index(inplace=True)
+    print(groups)
+
