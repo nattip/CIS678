d87e220dc0744d07c5ae6b6630dc1d159e691274
Natalie Tipton <tiptonna@mail.gvsu.edu>
2020-03-12 20:55:08 -0400

    added documentation

--- project_3/id3_v2.py
+++ project_3/id3_v2.py
@@ -1,5 +1,5 @@
 # Written by Natalie Tipton
-# March 10, 20202
+# March 12, 20202
 # CIS 678 - Machine Learning
 # Dr. Greg Wolffe
 # ID3 decision tree creator
@@ -10,9 +10,11 @@ import matplotlib.pyplot as plt
 import copy
 from networkx import drawing
 
+##########################################################################
+# read in metadata from data set then all lines of data
+
 
 def open_data(filename):
-    # open dataset
     with open(filename) as f:
 
         # read in metaData for dataset
@@ -29,10 +31,16 @@ def open_data(filename):
 
         numData = int(f.readline())
 
+        # read in actual data
         for i in range(numData):
             data[i] = f.readline().rstrip().split(",")
 
     return numClasses, classes, numAttr, factors, attr, numData, data
+
+
+##########################################################################
+# count the number of times each classfication label was found
+# for the total data set as well as for each individual factor
 
 
 def class_counts(data, numClasses, numAttr, numData, classes):
@@ -55,6 +63,10 @@ def class_counts(data, numClasses, numAttr, numData, classes):
     return total_class_counts, factor_counts
 
 
+##########################################################################
+# Calculate set entropy of entire dataset
+
+
 def find_set_entropy(total_class_counts, numClasses, numData):
     set_entropy = 0
     for i in range(numClasses):
@@ -64,15 +76,23 @@ def find_set_entropy(total_class_counts, numClasses, numData):
     return set_entropy
 
 
+##########################################################################
+# calculate entropies of each factor in dataset
+
+
 def find_entropy(counts, numClasses, factors):
     entropy = {}
 
+    # loop through all factors
     for idx in factors:
         entr = 0
         numData = 0
 
+        # count up total occurrences of current factor in the data
         for i in range(numClasses):
             numData += counts[classes[i]][idx]
+
+        # if there are no occurrences, entropy will be 0
         if numData == 0:
             entropy[idx] = 0
             continue
@@ -80,6 +100,7 @@ def find_entropy(counts, numClasses, factors):
             prob = counts[classes[i]][idx] / numData
             entr -= prob * np.log2(prob)
 
+        # fix for when log2 returns NaN
         if np.isnan(entr):
             entr = 0
         entropy[idx] = entr
@@ -87,14 +108,22 @@ def find_entropy(counts, numClasses, factors):
     return entropy
 
 
+##########################################################################
+# calculate gains for each attribute in dataset
+
+
 def find_gain(entropy, counts, numData, numClasses, attr, set_entropy, factors):
     gain = {}
+
+    # loop through all attributes
     for key in attr.keys():
         g = 0
+        # loop through the factors for each attribute
         for factor in attr[key]:
-            if factor not in factors:
-                continue
+            # if factor not in factors:
+            #     continue
             num_factor = 0
+            # count all occurrences of that factor in the current dataset
             for i in range(numClasses):
                 num_factor += counts[classes[i]][factor]
 
@@ -103,6 +132,11 @@ def find_gain(entropy, counts, numData, numClasses, attr, set_entropy, factors):
         gain[key] = g + set_entropy
 
     return gain
+
+
+##########################################################################
+# find the root node of the tree based on which attribute
+# has the highest gain
 
 
 def find_root_node():
@@ -119,16 +153,27 @@ def find_root_node():
     return root, entropy
 
 
+##########################################################################
+# recursive function that follows down each individual branch
+# and finds every node until a final classification can be made
+
+
 def find_next_node(node, counts, numData, attr, numClasses, entropy, data):
 
+    # loop through the branches off of the root node
     for branch in attr[node]:
         add_to_temp_data = []
         temp_data = {}
-        bounds = []
         temp_factors = []
-        # print(branch)
+        bounds = []
 
+        # list of the factors that a data point needs
+        # to be included down the branch
         bounds.append(branch)
+
+        # loop through all data to find which data points
+        # satisfy the location in the tree and add them
+        # to the temporary dataset
         for i in range(numData):
             for j in range(numAttr):
                 if data[i][j] in bounds:
@@ -137,35 +182,35 @@ def find_next_node(node, counts, numData, attr, numClasses, entropy, data):
         for temp in range(len(add_to_temp_data)):
             temp_data[temp] = data[add_to_temp_data[temp]]
 
-        # print(temp_data)
         temp_factors = [e for e in factors if e not in attr[node]]
 
         temp_attr = copy.deepcopy(attr)
         del temp_attr[node]
 
+        # find updated counts for each classificaiton
         temp_total_counts, temp_factor_counts = class_counts(
             temp_data, numClasses, numAttr, len(add_to_temp_data), classes
         )
 
         zeros = 0
-
         for label in classes:
+            # if data with a certain factor is never labelled
+            # one of the possible classifications, count it
             if temp_factor_counts[label][branch] == 0:
                 zeros += 1
             else:
                 pos_factor = label
 
+        # if a factor only exist as one classification, end node
+        # with classification label
         if zeros == (numClasses - 1):
             print("Next node =", pos_factor)
+            # tree[root][branch] = pos_factor
             continue
 
-        # print(len(add_to_temp_data))
-        # print(temp_factor_counts)
+        # determine set entropy, factor entropies, and gain
         set_entropy = entropy[branch]
         temp_entropy = find_entropy(temp_factor_counts, numClasses, temp_factors)
-
-        # print("entropy =", temp_entropy)
-
         temp_gain = find_gain(
             temp_entropy,
             temp_factor_counts,
@@ -176,9 +221,13 @@ def find_next_node(node, counts, numData, attr, numClasses, entropy, data):
             temp_factors,
         )
 
+        # decide next node
         next_node = max(temp_gain, key=temp_gain.get)
         print("Next node =", next_node)
+        # tree[root][branch] = next_node
 
+        # call function again to find next node given
+        # current information
         find_next_node(
             next_node,
             temp_factor_counts,
@@ -189,9 +238,10 @@ def find_next_node(node, counts, numData, attr, numClasses, entropy, data):
             temp_data,
         )
 
-        # root = max(gain, key=gain.get)
 
+##########################################################################
 
+# global structures
 classes = []
 attr = {}
 data = {}
@@ -200,14 +250,20 @@ line = []
 tree = {}
 
 if __name__ == "__main__":
-
+    # open and read data
     numClasses, classes, numAttr, factors, attr, numData, data = open_data(
-        "./contact_data.txt"
+        "./fishing_data.txt"
     )
+
+    # count up intial occurrences of each class
     total_class_counts, factor_counts = class_counts(
         data, numClasses, numAttr, numData, classes
     )
+
+    # find the first node of the tree
     root, entropy = find_root_node()
 
+    # build entire tree
     find_next_node(root, factor_counts, numData, attr, numClasses, entropy, data)
+    print(tree)
 
