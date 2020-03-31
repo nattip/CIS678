f01b52a87dbd5f8e1960bdfb18f05e2179ca4eb0
Jarred Parr <jparr721@gmail.com>
2020-03-30 18:26:27 -0600

    fixed the neural network

--- project_4/neural_net.py
+++ project_4/neural_net.py
@@ -6,24 +6,36 @@
 
 import numpy as np
 from numpy import exp, array, random, dot
+from collections import Counter
 
 
-class neuralNet:
+class NeuralNet:
     def __init__(self):
         random.seed(1)
 
         self.n_hidden = 10
         self.lr = 0.0001
 
-        self.weights_hidden = random.normal(0, 1, (inputs.shape[1], self.n_hidden))
+        self.weights_hidden = random.normal(
+            0, 1, (inputs.shape[1], self.n_hidden)
+        )
         self.bias_hidden = np.zeros(self.n_hidden)
 
         self.n_output = n_outputs
-        self.weights_output = random.normal(0, 1, (self.n_hidden, self.n_output))
+        print(self.n_output, n_outputs)
+        self.weights_output = random.normal(
+            0, 1, (self.n_hidden, self.n_output)
+        )
         self.bias_output = np.zeros(self.n_output)
 
+    def encode_targets(self, targets, n_classes):
+        onehot = np.zeros((n_classes, targets.shape[0]))
+        for i, v in enumerate(targets.astype(int)):
+            onehot[v, i] = 1.0
+        return onehot.T
+
     def sigmoid(self, x):
-        return 1 / (1 + exp(-x))
+        return 1 / (1 + exp(-np.clip(x, -250, 250)))
 
     def deriv_sigmoid(self, x):
         return x * (1 - x)
@@ -32,46 +44,44 @@ class neuralNet:
         hidden_out = dot(inputs, self.weights_hidden) + self.bias_hidden
         hidden_sigmoid = self.sigmoid(hidden_out)
 
-        output_out = dot(hidden_sigmoid, self.weights_output) + self.bias_output
+        output_out = (
+            dot(hidden_sigmoid, self.weights_output) + self.bias_output
+        )
         output_sigmoid = self.sigmoid(output_out)
 
         return hidden_sigmoid, output_sigmoid
 
     def train(self, training_inputs, targets, iterations):
+        targets = self.encode_targets(targets, 10)
         for iteration in range(iterations):
             hidden_sigmoid, output_sigmoid = self.forward(training_inputs)
 
-            delta_output = output_sigmoid.T - targets
+            delta_output = output_sigmoid - targets
 
             deriv_sig_hidden = self.deriv_sigmoid(hidden_sigmoid)
-            delta_hidden = dot(delta_output.T, self.weights_output.T) * deriv_sig_hidden
+            delta_hidden = (
+                dot(delta_output, self.weights_output.T) * deriv_sig_hidden
+            )
 
             grad_weight_hidden = dot(training_inputs.T, delta_hidden)
             grad_bias_hidden = np.sum(delta_hidden, axis=0)
+            # print(grad_weight_hidden, "BIAS", grad_bias_hidden)
 
-            grad_weight_output = dot(hidden_sigmoid.T, delta_output.T)
-            grad_bias_output = np.sum(delta_output.T, axis=0)
-
-            # print(grad_weight_output)
-            # print(grad_weight_hidden)
+            grad_weight_output = dot(hidden_sigmoid.T, delta_output)
+            grad_bias_output = np.sum(delta_output, axis=0)
+            # print(grad_weight_output, "BIAS", grad_bias_output)
 
             self.weights_hidden -= self.lr * grad_weight_hidden
-            self.bias_hidden -= self.lr + grad_bias_hidden
+            self.bias_hidden -= self.lr * grad_bias_hidden
 
             self.weights_output -= self.lr * grad_weight_output
-            self.bias_output -= self.lr + grad_bias_output
-
-            # print(self.weights_output[0][0])
-            # print(self.weights_hidden[0][0])
+            self.bias_output -= self.lr * grad_bias_output
 
     def test(self, testing_inputs):
-        (hidden_sigmoid_test, output_sigmoid_test,) = self.forward(testing_inputs)
-        # print(output_sigmoid_test.shape)
-        # print(output_sigmoid_test)
+        hidden_sigmoid_test, output_sigmoid_test = self.forward(testing_inputs)
 
-        # prediction = np.argmax(output_sigmoid_test, axis=1)
-        # print(prediction)
-        print(output_sigmoid_test)
+        prediction = np.argmax(output_sigmoid_test, axis=1)
+        return prediction
 
 
 if __name__ == "__main__":
@@ -86,7 +96,9 @@ if __name__ == "__main__":
                 inputs = array([line.rstrip().split(" ")[:-1]])
                 flag = 1
             else:
-                targets = np.append(targets, array([line.rstrip().split(" ").pop()]))
+                targets = np.append(
+                    targets, array([line.rstrip().split(" ").pop()])
+                )
                 inputs = np.concatenate(
                     (inputs, array([line.rstrip().split(" ")[:-1]]))
                 )
@@ -98,8 +110,8 @@ if __name__ == "__main__":
     n_outputs = len(class_labels)
     n_features = inputs.shape[1]
 
-    nn = neuralNet()
-    nn.train(inputs, targets, 500)
+    nn = NeuralNet()
+    nn.train(inputs, targets, 100)
 
     with open("./digits-test.data") as f:
         lines = f.readlines()
@@ -121,5 +133,4 @@ if __name__ == "__main__":
     inputs_test = array(inputs_test, dtype=np.float64)
     targets_test = array(targets_test, dtype=np.float64)
 
-    for data in range(0, len(inputs_test)):
-        nn.test(inputs_test[data])
+    print(Counter(nn.test(inputs_test)))
